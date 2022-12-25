6934795cc9d83155bd8873a488cdd2931e2bb4bb
Jasneet <jasneetpalsingh619@gmail.com>
2022-12-24 22:01:32 -0700

    created a login page for admin access

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ function openMenu(){
 function closeMenu(){
     document.body.classList.remove("menu--open")
 }
+
+
+// LOGIN //
+
+
+
+function setFormMessage(formElement, type, message){
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error")
+    messageElement.classList.add(`form__message--${type}`)
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const loginForm = document.querySelector("#login");
+
+
+    loginForm.addEventListener("submit", e=>{
+        e.preventDefault();
+
+
+        // Perform AJAX/Fetch Login
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
+    })
+
+    
+})
